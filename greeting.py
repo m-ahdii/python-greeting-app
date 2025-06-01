37c149595b0d708489f6d10f09be59120892edab
m-ahdii <mmmoradkhani@gmail.com>
2025-06-01 15:59:30 +0300

    Initial commit: Add greeting.py script

---
+++ greeting.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+day = input("Enter the current day of the week: ")
+print(f"Hello, {name}! Happy {day}!")
+
+def get_feedback():
+    feedback = input("What do you think about this greeting program? ")
+    print("Thank you for your feedback!")
+    
+get_feedback()
